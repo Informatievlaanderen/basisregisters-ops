b375d622fcfe98aebe920dc10c327e9c3971cc59
pgallik <patrickgallik@gmail.com>
2023-08-21 13:13:44 +0200

    fix: extract auth settings into separate section from joboptions

---
+++ src/Ops.Web/Jobs/AuthOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Ops.Web.Jobs;
+
+public class AuthOptions
+{
+    public string TokenEndpoint { get; set; }
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+}
