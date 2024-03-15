73d7ed955ac77ed42103f9fe1d7906a7102102db
emalfroy <egon.malfroy@gmail.com>
2024-03-15 10:19:45 +0100

    feat: move to dotnet 8.0.2
    BREAKING CHANGE: move to dotnet 8.0.2

---
+++ src/Ops.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Basisregisters Vlaanderen")]
+[assembly: AssemblyDescription("Ops")]
+[assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
+[assembly: AssemblyCompany("Vlaamse overheid")]
+[assembly: Guid("d320190a-d6ce-4169-b7f9-8884acc3229c")]
+[assembly: NeutralResourcesLanguage("en")]
